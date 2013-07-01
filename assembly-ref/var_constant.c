70d9f31f85968377e1d2e900b698f11324ef124e
Julio Ribeiro <wwwjuliords@gmail.com>
2013-07-01 10:06:40 +0000

    assembly ref

---
+++ assembly-ref/var_constant.c
@@ -0,0 +1,10 @@
+
+int f()
+{
+	int i = 0;
+	char c = 'a';
+	float f = 1.2;
+	puts("olamundo");
+
+	return i + c + (int)f;
+}
---
+++ assembly-ref/var_local.c
@@ -0,0 +1,6 @@
+
+int f()
+{
+	int v1, v2, v3, v4, v5, v6, v7;
+	return v1 + v2 + v3 + v4 + v5 + v6 + v7;
+}
